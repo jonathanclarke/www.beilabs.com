c5ed5ec84a9997edeb27cf27e0db2fd2393f4b60
Jonathan Clarke <jonathan@beilabs.com>
2010-06-25 18:47:26 +1000

    Adding production deployment + removing default index

---
+++ config/deploy/production.rb
@@ -0,0 +1,4 @@
+set :deploy_to, "/u/apps/www.beilabs.com"
+set :rails_env, "production"
+set :default_env, "production"
+
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-set :deploy_to, "/u/apps/events"
+set :deploy_to, "/u/apps/www.beilabs.com"
 set :rails_env, "staging"
 set :default_env, "staging"
 
