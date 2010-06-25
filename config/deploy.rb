4dcff6217c3c6452cede6751f7f991fa6040b6d0
Jonathan Clarke <jonathan@beilabs.com>
2010-06-25 18:45:28 +1000

    Adding deployment script

---
+++ config/deploy.rb
@@ -0,0 +1,57 @@
+
+set :stages, %w(staging production)
+set :default_stage, "production"
+require 'capistrano/ext/multistage'
+
+set :application, "www.beilabs.com"
+default_run_options[:pty] = true
+
+set :user, "jonathan"
+set :scm, "git"
+set :branch, "master"
+set :repository,  "git@github.com:westwickfarrow/events.wf.net.au"
+set :port, 22
+
+role :web, "www.beilabs.com"                          # Your HTTP server, Apache/etc
+role :app, "www.beilabs.com"                          # This may be the same as your `Web` server
+role :db,  "www.beilabs.com", :primary => true        # This is where Rails migrations will run
+
+after "deploy:update_code", "bundler:bundle_new_release", "deploy:symbolic_links"
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  desc "Make symlink for database yaml"
+  task :symbolic_links do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(current_release, '.bundle')
+    run("mkdir -p #{shared_dir}")
+    run "ln -nfs #{shared_path}/bundle #{release_path}/.bundle"
+    run "ln -nfs #{shared_path}/bundler_gems #{release_path}/vendor/bundler_gems"
+  end
+
+  task :bundle_new_release, :roles => :app do
+    puts "Installing bundler gems"
+    bundler.create_symlink
+    run "cd #{release_path} && bundle install vendor/bundler_gems"
+  end
+
+  task :after_update_code, :roles => [:app, :db, :web] do
+
+    run "cd #{release_path} && rake asset:packager:build_all"
+    run "for file in $(find #{release_path}/public/javascripts \\
+      -name '*.js'); do gzip $file -f -c > $file.gz; done"
+    run "for file in $(find #{release_path}/public/stylesheets \\
+      -name '*.css'); do gzip $file -f -c > $file.gz; done"
+  end
+end
---
+++ config/deploy/staging.rb
@@ -0,0 +1,4 @@
+set :deploy_to, "/u/apps/events"
+set :rails_env, "staging"
+set :default_env, "staging"
+
