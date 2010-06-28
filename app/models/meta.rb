fd1039d98c2024395ec8a2c95cc0414a6aad945c
Jonathan Clarke <jonathan@beilabs.com>
2010-06-28 16:51:31 +1000

    Cleaning up the footer

--- app/models/meta.rb
+++ app/models/meta.rb
@@ -5,7 +5,7 @@ class Meta < ActiveRecord::Base
   def self.last_commit
     begin
       commits = GitHub::API.commits('beilabs','www.beilabs.com')
-      File.open("#{RAILS_ROOT}/app/views/shared/_last_commit.html.haml", 'w') {|f| f.write(time_ago_in_words( commits.first.committed_date[0..9].to_date )) }
+      File.open("#{RAILS_ROOT}/app/views/shared/_last_commit.html.haml", 'w') {|f| f.write("This site was last updated about #{ time_ago_in_words( commits.first.committed_date[0..9].to_date ) } ago") }
     rescue Exception => e
       logger.error "An exception occured when saving the file #{e.to_s}"
     end
