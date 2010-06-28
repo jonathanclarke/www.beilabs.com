37ba385608d5c2ed2c62e104444758ac1f422a69
Jonathan Clarke <jonathan@beilabs.com>
2010-06-28 16:47:22 +1000

    Last commit integration with github for footer

---
+++ app/models/meta.rb
@@ -0,0 +1,13 @@
+require 'action_view/helpers/date_helper'
+include ActionView::Helpers::DateHelper
+
+class Meta < ActiveRecord::Base
+  def self.last_commit
+    begin
+      commits = GitHub::API.commits('beilabs','www.beilabs.com')
+      File.open("#{RAILS_ROOT}/app/views/shared/_last_commit.html.haml", 'w') {|f| f.write(time_ago_in_words( commits.first.committed_date[0..9].to_date )) }
+    rescue Exception => e
+      logger.error "An exception occured when saving the file #{e.to_s}"
+    end
+  end
+end
