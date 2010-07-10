f36ad68abff00cd1d1d96cb009708a72b2a16859
Jonathan Clarke <jonathan@beilabs.com>
2010-07-10 11:57:45 +1000

    Setting correct timezone

--- app/models/meta.rb
+++ app/models/meta.rb
@@ -5,7 +5,8 @@ class Meta < ActiveRecord::Base
   def self.last_commit
     begin
       commits = GitHub::API.commits('beilabs','www.beilabs.com')
-      File.open("#{RAILS_ROOT}/app/views/shared/_last_commit.html.haml", 'w') {|f| f.write("This site was last updated about #{ time_ago_in_words( commits.first.committed_date[0..9].to_date ) } ago") }
+      message2 = "#{ Time.now } - This site was last updated about #{ time_ago_in_words( commits.first.committed_date.to_time + 7.hours ) } ago"
+      File.open("#{RAILS_ROOT}/app/views/shared/_last_commit.html.haml", 'w') {|f| f.write( message ) }
     rescue Exception => e
       logger.error "An exception occured when saving the file #{e.to_s}"
     end
--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.time_zone = 'UTC'
+  config.time_zone = 'Sydney'
 end
 Sass::Plugin.options[:style] = :compressed
 Haml::Template.options[:format] = :html5
