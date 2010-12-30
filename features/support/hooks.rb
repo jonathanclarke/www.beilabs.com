3fe49d519f67b2791b164fdd9f9c45937d85d4ec
Jonathan Clarke <jonathan@beilabs.com>
2010-12-30 23:03:55 +1100

    Removing older files

--- features/support/hooks.rb
+++
@@ -1,6 +0,0 @@
-AfterStep('@validations') do |scenario|
-  response = Validator.nu(body)
-  result = (response.include? "error")==false
-  puts "This page is not valid: #{ response.inspect }\n#{ body.to_s }" if result==false
-  assert result == true
-end
