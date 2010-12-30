0056d613287903e1ee16f6d1a119aceba3cc8786
Jonathan Clarke <jonathan@beilabs.com>
2010-12-30 22:53:30 +1100

    New files for upgrade to rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WwwBeilabsCom::Application.config.secret_token = 'd7775bd0c2d8447048b77e010930452de68135febf1921a6f2d862128b85b31656534f3b0a69e2cd2ae9c1d7f75cf40a38a8c18ab23f3dae415b04dcd160d2f9'
